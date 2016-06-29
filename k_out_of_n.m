256f9c2aab0bfdb98cf6d2c2c4dc144cddc4beac
vivian <guojingbochina@gmail.com>
2016-06-29 15:11:23 -0400

    k out of n: Good system

--- k_out_of_n.m
+++ k_out_of_n.m
@@ -2,7 +2,7 @@ function [flag] = k_out_of_n (k,W1)
 sw1 = size(W1);
 i = sw1(1)-1;
 for l = 1 : 1 : i + 1
-    if sum(W1(l,:)==0)/2 > k 
+    if sum(W1(l,:)~=0)/2 > k 
         flag = 0;
         break
     end
